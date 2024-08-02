03c357204fb2d406b9a8cee601d07c7c112fd661
Matiki <mattiikkii@gmail.com>
2024-08-02 17:08:01 +0200

    Add SnackBar Channel

--- lib/src/menu.dart
+++ lib/src/menu.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-
 enum _MenuOptions {
   navigationDelegate,
-  userAgent,                                              // Add this line
+  userAgent,
+  javascriptChannel,                                      // Add this option
 }
 
-class Menu extends StatefulWidget {                       // Convert to StatefulWidget
+class Menu extends StatefulWidget {
   const Menu({required this.controller, super.key});
 
   final WebViewController controller;
 
-  @override                                               // Add from here
+  @override
   State<Menu> createState() => _MenuState();
 }
 
-class _MenuState extends State<Menu> {                    // To here.
+class _MenuState extends State<Menu> {
   @override
   Widget build(BuildContext context) {
     return PopupMenuButton<_MenuOptions>(
       onSelected: (value) async {
         switch (value) {
-          case _MenuOptions.navigationDelegate:           // Modify from here
+          case _MenuOptions.navigationDelegate:
             await widget.controller
                 .loadRequest(Uri.parse('https://youtube.com'));
           case _MenuOptions.userAgent:
@@ -30,7 +30,20 @@ class _MenuState extends State<Menu> {                    // To here.
             if (!context.mounted) return;
             ScaffoldMessenger.of(context).showSnackBar(SnackBar(
               content: Text('$userAgent'),
-            ));                                           // To here.
+            ));
+          case _MenuOptions.javascriptChannel:            // Add from here
+            await widget.controller.runJavaScript('''
+var req = new XMLHttpRequest();
+req.open('GET', "https://api.ipify.org/?format=json");
+req.onload = function() {
+  if (req.status == 200) {
+    let response = JSON.parse(req.responseText);
+    SnackBar.postMessage("IP Address: " + response.ip);
+  } else {
+    SnackBar.postMessage("Error: " + req.status);
+  }
+}
+req.send();''');                                          // To here.
         }
       },
       itemBuilder: (context) => [
@@ -38,9 +51,13 @@ class _MenuState extends State<Menu> {                    // To here.
           value: _MenuOptions.navigationDelegate,
           child: Text('Navigate to YouTube'),
         ),
-        const PopupMenuItem<_MenuOptions>(                // Add from here
+        const PopupMenuItem<_MenuOptions>(
           value: _MenuOptions.userAgent,
           child: Text('Show user-agent'),
+        ),
+        const PopupMenuItem<_MenuOptions>(                // Add from here
+          value: _MenuOptions.javascriptChannel,
+          child: Text('Lookup IP Address'),
         ),                                                // To here.
       ],
     );
